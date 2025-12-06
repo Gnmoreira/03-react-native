0519249454be179b52c3d00cc000751734adcdd2
Gnmoreira <107262839+Gnmoreira@users.noreply.github.com>
2025-12-06 18:06:37 +0000

    exemplo

--- meu-app/App.tsx
+++ meu-app/App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+//importa barra de status do expo 
 import { StyleSheet, Text, View } from 'react-native';
+//importa componentes do react native
 import TelaDaListaDeTarefas from './components/TelaDaListaDeTarefas';
 export default function App() {
   return (
+    // visao principal que vai envolver toda a interface
     <View style={styles.container}>
       <TelaDaListaDeTarefas />
       <StatusBar style="auto" />
--- meu-app/components/TelaDaListaDeTarefas.tsx
+++ meu-app/components/TelaDaListaDeTarefas.tsx
@@ -1,26 +1,36 @@
 import { useEffect, useState } from 'react';
+//importa hooks 
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { tarefasMockadas } from '../data/tarefas';
+//importa tarefasmockadas do data/tarefas
 import { Tarefa } from '../types/Tarefa';
 export default function TelaDaListaDeTarefas() {
+
     const [tarefas, setTarefas] = useState<Tarefa[]>([]);
     const [carregando, setCarregando] = useState(true);
     useEffect(() => {
+        //simula o um carregamento das tarefas por 2 segundos , enfase no },2000
         const timer = setTimeout(() => {
             setTarefas(tarefasMockadas);
+            //carrega as tarefas (importando do types, tarefas)
             setCarregando(false);
+            
         }, 2000);
         return () => clearTimeout(timer);
+        //desativa o carregamento caso o carregamento acabe antes do timeout
     }, []);
+    //o array e vazio para garatir que que seja rodado apenas uma vez 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Minhas Tarefas</Text>
             {carregando ? (
                 <Text>Carregando...</Text>
+                // confere se esta carregando e se estiver mostra o texto "carregando"
             ) : (
                 <FlatList
                     data={tarefas}
                     keyExtractor={(item) => item.id.toString()}
+                    //transforma o id do item  em uma string
                     renderItem={({ item }) => <Text style={styles.item}>{item.id} -
 
                         {item.titulo}</Text>}
